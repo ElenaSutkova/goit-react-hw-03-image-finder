a72f3b2a65a1d3e6908d54cb84d9bbc2b70b1546
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2023-12-18 15:04:58 +0200

    btn

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import styles from '../Button/Button.module.css'
+
+class Button extends Component {
+    handleClick = (event) => {
+        event.preventDefault();
+        const { onClick } = this.props;
+        onClick();
+    }
+
+    render() {
+        return (
+            <button
+                className={styles.button}
+                onClick={this.handleClick}>
+                Load More
+            </button>
+        )
+    }
+}
+
+export default Button;
